ed322ff8e57353105698a0160995d0769e69e232
Sajeeb <sajeebahamed6@gmail.com>
2020-03-11 02:34:18 +0600

    dynamic data to components/props

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const developers = ['John', 'Smith', 'Albid', 'Jhankar'];
   return (
     <div className="App">
       <header className="App-header">
-        <MyInfo> </MyInfo>
-        <MyInfo> </MyInfo>
-        <MyInfo> </MyInfo>
-        <MyInfo> </MyInfo>
+        <MyInfo name= {developers[0]} des = "Im a frontend developer living in dhaka bangladesh"> </MyInfo>
+        <MyInfo name = {developers[1]}></MyInfo>
+        <MyInfo name="Shakib" des= "Im a backend developer living in dhaka bangladesh"> </MyInfo>
+        
       </header>
     </div>
   );
 }
-function MyInfo(){
+function MyInfo(props){
   const myStyle = {
     backgroundColor : 'green',
     border : '1px solid red',
@@ -24,8 +25,8 @@ function MyInfo(){
   return (
   // <div style = {{border : "1px solid red", margin : "10px"}}>
   <div style={myStyle}>
-    <h1>Hello, This is Sajeeb</h1>
-    <h3>Im a frontend developer living in dhaka bangladesh</h3>
+    <h1>Hello, This is {props.name}</h1>
+    <h3> {props.des} </h3>
   </div>
   )
 }
