ab4a2de6aa6a8c2b3f9ba276badaa89ec459b2c4
Sajeeb <sajeebahamed6@gmail.com>
2020-03-11 02:01:58 +0600

    component/jsx

--- src/App.js
+++ src/App.js
@@ -6,21 +6,28 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello React
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <MyInfo> </MyInfo>
+        <MyInfo> </MyInfo>
+        <MyInfo> </MyInfo>
+        <MyInfo> </MyInfo>
       </header>
     </div>
   );
+}
+function MyInfo(){
+  const myStyle = {
+    backgroundColor : 'green',
+    border : '1px solid red',
+    margin : '10px',
+    padding : '10px'
+  }
+  return (
+  // <div style = {{border : "1px solid red", margin : "10px"}}>
+  <div style={myStyle}>
+    <h1>Hello, This is Sajeeb</h1>
+    <h3>Im a frontend developer living in dhaka bangladesh</h3>
+  </div>
+  )
 }
 
 export default App;
